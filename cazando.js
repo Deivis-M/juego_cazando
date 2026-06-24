9ffeca1bc025b1fcf5e2abeef5d69dd015dc0911
Deivis-M <nitaballenita09@gmail.com>
2026-06-24 11:18:18 -0500

    funcion:graficarComida-InciarJuego y nuevas variables

--- cazando.js
+++ cazando.js
@@ -1,11 +1,27 @@
 let canvas=document.getElementById("areaJuego")
 let ctx =canvas.getContext("2d")
-let valorAnchoRectangulo=150
-let valorAltoRectangulo=85
-const VALOR_X=(canvas.width/2)-(valorAnchoRectangulo/2)
-const VALOR_Y=(canvas.height/2)-(valorAltoRectangulo/2)
+const ANCHO_GATO=200
+const ALTO_GATO=100
+let gatoX=(canvas.width/2)-(ANCHO_GATO/2)
+let gatoY=(canvas.height/2)-(ALTO_GATO/2)
+
+const ANCHO_COMIDA=50
+const ALTO_COMIDA=50
+let comidaX=0
+let comidaY=0
+
+
+function iniciarJuego(){
+    graficarGato()
+    graficarComida()
+}
 
 function graficarGato(){
     ctx.fillStyle ="#0bfa0b";
-    ctx.fillRect(VALOR_X,VALOR_Y,valorAnchoRectangulo,valorAltoRectangulo);
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
+}
+
+function graficarComida(){
+    ctx.fillStyle ="#f6fa0b";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);   
 }
