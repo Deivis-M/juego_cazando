8953ccb77e65fa07625f0199fe74af96bba4f764
Deivis-M <nitaballenita09@gmail.com>
2026-06-25 16:05:07 -0500

    funcion detectarColision

--- cazando.js
+++ cazando.js
@@ -1,12 +1,12 @@
 let canvas=document.getElementById("areaJuego")
 let ctx =canvas.getContext("2d")
-const ANCHO_GATO=200
-const ALTO_GATO=100
+const ANCHO_GATO=100
+const ALTO_GATO=50
 let gatoX=(canvas.width/2)-(ANCHO_GATO/2)
 let gatoY=(canvas.height/2)-(ALTO_GATO/2)
 
-const ANCHO_COMIDA=50
-const ALTO_COMIDA=50
+const ANCHO_COMIDA=30
+const ALTO_COMIDA=30
 let comidaX=0
 let comidaY=0
 
@@ -38,6 +38,7 @@ function moverIzquierda(){
     limpiarCanva()
     graficarGato()
     graficarComida()
+    detectarColision()
 }
 
 function moverDerecha(){
@@ -45,6 +46,7 @@ function moverDerecha(){
     limpiarCanva()
     graficarGato()
     graficarComida()   
+    detectarColision()
 }
 
 function moverArriba(){
@@ -52,11 +54,21 @@ function moverArriba(){
     limpiarCanva()
     graficarGato()
     graficarComida()  
+    detectarColision()
 }
 
 function moverAbajo(){
     gatoY=gatoY+10
     limpiarCanva()
     graficarGato()
-    graficarComida()   
+    graficarComida()
+    detectarColision()  
+}
+
+function detectarColision(){
+    if (comidaX+ANCHO_COMIDA>gatoX && comidaY+ALTO_COMIDA>gatoY  
+        && comidaX<gatoX+ANCHO_GATO && comidaY<gatoY+ALTO_GATO)
+        {
+        alert ("Colision")
+    }
 }
